1d05f193610686dd2a3d030f4eca43a68b767380
MangoTechSolutions <sales.mangotechsolutions@gmail.com>
2022-03-21 16:08:52 +0500

    SideBar

---
+++ components/CustomDrawer.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { StyleSheet, Image, View, Text } from 'react-native'
+import { DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+
+
+
+const CustomDrawer = (props) => {
+    return (
+        <>
+            <View style={{ backgroundColor: '#C4B454', minHeight: 170, marginBottom: 20, justifyContent: 'center', borderBottomLeftRadius: 20, borderBottomRightRadius: 20 }}>
+                <Image source={require('../assets/avatar.png')} style={{ height: 80, width: 80, borderRadius: 100, marginLeft: 20 }} />
+                <Text style={{ fontSize: 20, fontWeight: '400', paddingLeft: 25 }}>User 1</Text>
+                <Text style={{ fontSize: 12, fontWeight: '500', paddingLeft: 25 }}>abc@gmail.com</Text>
+            </View>
+
+            <DrawerContentScrollView {...props}>
+                <View style={{ flex: 1 }}>
+                    <DrawerItemList  {...props} />
+                </View>
+            </DrawerContentScrollView>
+
+            <View style={{ fontSize: 20, fontWeight: 'bold', paddingVertical: 20, paddingHorizontal: 15, backgroundColor: '#C4B454', borderTopLeftRadius: 20, borderTopRightRadius: 20 }}>
+                <TouchableOpacity>
+                    <Text style={{ fontSize: 16, fontWeight: 'bold', color: '#FFFFF0' }}> Log Out</Text>
+                </TouchableOpacity>
+            </View>
+        </>
+    )
+}
+
+export default CustomDrawer
+
+const styles = StyleSheet.create({})
